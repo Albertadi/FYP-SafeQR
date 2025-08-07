9f97b4fe6fecc1973be3d2d9f42ee0773d4083a6
Albertadi <albertadi412@gmail.com>
2025-08-07 12:29:28 +0800

    fixed callback missing token error'

--- app/callback.tsx
+++ app/callback.tsx
@@ -1,44 +1,14 @@
 "use client"
 
-import { supabase } from "@/utils/supabase"
-import { useLocalSearchParams, useRouter } from "expo-router"
+import { useRouter } from "expo-router"
 import { useEffect } from "react"
-import { Alert } from "react-native"
 
 export default function CallbackScreen() {
   const router = useRouter()
-  const { token, type } = useLocalSearchParams()
 
   useEffect(() => {
     const handleAuthRedirect = async () => {
-      if (!token || typeof token !== "string") {
-        Alert.alert("Missing or invalid token")
-        router.replace("/(tabs)/register")
-        return
-      }
-
-      const { data, error } = await supabase.auth.exchangeCodeForSession(token)
-
-      if (error) {
-        Alert.alert("Login failed", error.message)
-        router.replace("/(tabs)/register")
-        return
-      }
-
-      // Redirect based on type
-      if (type === "signup") {
-        Alert.alert("Email confirmed", "Your email has been confirmed. You can login now.")
-        router.replace("/(tabs)/register")
-      }
-      else if (type === "recovery") {
-        router.replace({
-          pathname: "/reset-password",
-          params: { token },
-        })
-      } else {
-        // fallback
-        router.replace("/(tabs)/register")
-      }
+      router.replace("/(tabs)/register")
     }
 
     handleAuthRedirect()
